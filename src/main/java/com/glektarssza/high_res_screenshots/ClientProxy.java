7f50b59e58160f0a500b66c140ed956cf1a3258f
G'lek Tarssza <glek@glektarssza.com>
2025-10-29 15:11:02 -0600

    Added a client-side proxy.

---
+++ src/main/java/com/glektarssza/high_res_screenshots/ClientProxy.java
@@ -0,0 +1,17 @@
+package com.glektarssza.high_res_screenshots;
+
+import java.lang.invoke.MethodHandles;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The client-side proxy.
+ */
+public class ClientProxy {
+    /**
+     * The logger for this class.
+     */
+    private static final Logger LOGGER = LoggerFactory
+        .getLogger(MethodHandles.lookup().lookupClass());
+}
